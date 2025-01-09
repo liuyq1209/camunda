1ff612f8253d7730449a791761a4ba30e406475c
Mustafa Dagher <mustafa.dagher@camunda.com>
2025-01-09 16:08:46 +0100

    feat: send incident notification async
    since it's async now, there's no guarantee it will be called immediately
    the test had to be adapted to await for the call to be verified

--- zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/handlers/IncidentHandler.java
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/handlers/IncidentHandler.java
@@ -31,6 +31,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -138,7 +139,7 @@ public class IncidentHandler implements ExportHandler<IncidentEntity, IncidentRe
     }
 
     if (Objects.equals(intent, IncidentIntent.CREATED)) {
-      incidentNotifier.notifyOnIncidents(List.of(entity));
+      CompletableFuture.runAsync(() -> incidentNotifier.notifyOnIncidents(List.of(entity)));
     }
   }
 
